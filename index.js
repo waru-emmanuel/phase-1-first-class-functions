ccea3832d2bf83ef43de33d337eef9e1546d8e62
wachira <wewaruts@gmail.com>
2024-03-29 01:18:27 +0300

    complete

---
+++ index.js
@@ -0,0 +1,28 @@
+//const { spy } = require("chai");
+
+function receivesAFunction(callback){
+    callback();
+  }
+  receivesAFunction(()=>{});
+
+  function returnsANamedFunction(){
+    return function name(){};  // naming a function
+  }
+
+  
+  function returnsAnAnonymousFunction(){
+    return function (){};  // a function without a name
+   }  
+  
+    
+
+
+
+
+
+
+
+
+
+
+   function waruts(){}
